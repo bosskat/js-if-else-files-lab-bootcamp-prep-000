61d195ad2b80a155e66ef768f9537f6fb1896736
winter mutt <sejongsan@hotmail.com>
2017-12-17 20:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var name = "Jane"
 var height = "70"
-if(name === "Jane"){
-  console.log("The name is correct.");
-} else {
-  console.log("Expected the name to be Jane.");
-}
+
 
 // Don't worry about this
 module.exports = { name, height
